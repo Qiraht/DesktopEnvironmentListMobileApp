5ac5402dcf257231199b0910f4f61af6bcbcd575
Thariq Aulia Akbar <thariq2712@gmail.com>
2020-10-25 17:32:09 +0700

    Add About Menu, Feature neede to be finished

---
+++ app/src/main/java/com/dicoding/qiraht/recyclerview/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.dicoding.qiraht.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        if (getActionBar() != null) {
+            getSupportActionBar().setTitle("About");
+        }
+    }
+}
--- app/src/main/java/com/dicoding/qiraht/recyclerview/MainActivity.java
+++ app/src/main/java/com/dicoding/qiraht/recyclerview/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -28,5 +31,26 @@ public class MainActivity extends AppCompatActivity {
         rvItem.setLayoutManager(new LinearLayoutManager(this));
         ListItemAdapter listItemAdapter = new ListItemAdapter(list);
         rvItem.setAdapter(listItemAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode (int selectedMode) {
+        switch (selectedMode) {
+            case R.id.action_about:
+                Intent moveIntent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(moveIntent);
+                break;
+        }
     }
 }
